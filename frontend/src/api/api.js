b370d6a43380eb3a8fe12eb134dd2667ad21497c
ChrispinMourus <chrispingnana@karunya.edu.in>
2025-10-29 21:13:38 +0530

    changes in deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Detect backend URL automatically
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api',
 });
 
 // Automatically attach token from localStorage
